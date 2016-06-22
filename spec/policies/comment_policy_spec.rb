2b49d0bee07064c769072c9f59f72f1e5f0e489b
gautamsawhney <gsawhney21@gmail.com>
2016-06-22 06:20:49 -0400

    Added Pundit Policies}

--- spec/policies/comment_policy_spec.rb
+++ spec/policies/comment_policy_spec.rb
@@ -6,41 +6,5 @@ describe CommentPolicy do
   let(:comment) { Comment.create }
 
   #add_here
-  context "being a/an mod" do
-    let(:user) {  User.create('roles' => 'mod', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
-    # Action Specs :: Mod
-    it { should permit_action(:destroy) }
-    it { should permit_action(:update) }
-    it { should permit_action(:show) }
-    it { should permit_action(:create) }
-    # Attribute Specs :: Mod
-    it { should permit_mass_assignment_of(:post_id) }
-    it { should permit_mass_assignment_of(:user_id) }
-    it { should permit_mass_assignment_of(:body) }
-  end
-  context "being a/an restaurant_owner" do
-    let(:user) {  User.create('roles' => 'restaurant_owner', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
-    # Action Specs :: RestaurantOwner
-    it { should permit_action(:destroy) }
-    it { should permit_action(:update) }
-    it { should permit_action(:show) }
-    it { should permit_action(:create) }
-    # Attribute Specs :: RestaurantOwner
-    it { should permit_mass_assignment_of(:post_id) }
-    it { should permit_mass_assignment_of(:user_id) }
-    it { should permit_mass_assignment_of(:body) }
-  end
-  context "being a/an admin" do
-    let(:user) {  User.create('roles' => 'admin', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
-    # Action Specs :: Admin
-    it { should permit_action(:destroy) }
-    it { should permit_action(:update) }
-    it { should permit_action(:show) }
-    it { should permit_action(:create) }
-    # Attribute Specs :: Admin
-    it { should permit_mass_assignment_of(:post_id) }
-    it { should permit_mass_assignment_of(:user_id) }
-    it { should permit_mass_assignment_of(:body) }
-  end
 
 end
--- spec/policies/post_policy_spec.rb
+++ spec/policies/post_policy_spec.rb
@@ -6,44 +6,5 @@ describe PostPolicy do
   let(:post) { Post.create }
 
   #add_here
-  context "being a/an mod" do
-    let(:user) {  User.create('roles' => 'mod', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
-    # Action Specs :: Mod
-    it { should permit_action(:destroy) }
-    it { should permit_action(:update) }
-    it { should permit_action(:show) }
-    it { should permit_action(:create) }
-    # Attribute Specs :: Mod
-    it { should permit_mass_assignment_of(:restaurant_id) }
-    it { should permit_mass_assignment_of(:user_id) }
-    it { should permit_mass_assignment_of(:title) }
-    it { should permit_mass_assignment_of(:body) }
-  end
-  context "being a/an restaurant_owner" do
-    let(:user) {  User.create('roles' => 'restaurant_owner', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
-    # Action Specs :: RestaurantOwner
-    it { should permit_action(:destroy) }
-    it { should permit_action(:update) }
-    it { should permit_action(:show) }
-    it { should permit_action(:create) }
-    # Attribute Specs :: RestaurantOwner
-    it { should permit_mass_assignment_of(:restaurant_id) }
-    it { should permit_mass_assignment_of(:user_id) }
-    it { should permit_mass_assignment_of(:title) }
-    it { should permit_mass_assignment_of(:body) }
-  end
-  context "being a/an admin" do
-    let(:user) {  User.create('roles' => 'admin', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
-    # Action Specs :: Admin
-    it { should permit_action(:destroy) }
-    it { should permit_action(:update) }
-    it { should permit_action(:show) }
-    it { should permit_action(:create) }
-    # Attribute Specs :: Admin
-    it { should permit_mass_assignment_of(:restaurant_id) }
-    it { should permit_mass_assignment_of(:user_id) }
-    it { should permit_mass_assignment_of(:title) }
-    it { should permit_mass_assignment_of(:body) }
-  end
 
 end
