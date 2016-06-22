313bd6a03a3f0bb7b28d7bb062308b8f90871bc4
gautamsawhney <gsawhney21@gmail.com>
2016-06-22 04:42:38 -0400

    Added Pundit Policy Specs

--- spec/policies/restaurant_policy_spec.rb
+++ spec/policies/restaurant_policy_spec.rb
@@ -6,5 +6,23 @@ describe RestaurantPolicy do
   let(:restaurant) { Restaurant.create }
 
   #add_here
+context "being a/an restaurant_owner" do
+                          let(:user) {  User.create('roles' => 'restaurant_owner', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
+                          # Action Specs :: RestaurantOwner
+it { should permit_action(:destroy) }
+it { should permit_action(:update) }
+it { should permit_action(:show) }
+it { should permit_action(:create) }
+                          # Attribute Specs :: RestaurantOwner
+                        end
+context "being a/an admin" do
+                          let(:user) {  User.create('roles' => 'admin', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
+                          # Action Specs :: Admin
+it { should permit_action(:destroy) }
+it { should permit_action(:update) }
+it { should permit_action(:show) }
+it { should permit_action(:create) }
+                          # Attribute Specs :: Admin
+                        end
 
 end
