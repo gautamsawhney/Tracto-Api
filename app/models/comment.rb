6e584bade62ff5f82bc09b4585575f9f63ff26c0
gautamsawhney <gsawhney21@gmail.com>
2016-06-22 04:36:56 -0400

    Added Comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,17 @@
+class Comment < ApplicationRecord
+
+  # Autocode: Callbacks
+    
+  # Autocode: Relationships
+      belongs_to :user
+      belongs_to :post
+  
+  # Autocode: Accept Nested Attributes
+
+  # File Upload
+
+  # Autocode: Validations
+
+	# Soft Destroy
+
+end
