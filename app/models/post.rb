7f34b3d6ac4a361f7efe05cb1aa7c3e17988f69e
gautamsawhney <gsawhney21@gmail.com>
2016-06-22 04:36:42 -0400

    Added Post model

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ApplicationRecord
+
+  # Autocode: Callbacks
+    
+  # Autocode: Relationships
+      belongs_to :user
+      belongs_to :restaurant
+  
+  # Autocode: Accept Nested Attributes
+
+  # File Upload
+
+  # Autocode: Validations
+
+	# Soft Destroy
+
+end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :post do
+    body "MyText"
+    title "MyString"
+    user nil
+    restaurant nil
+  end
+end
