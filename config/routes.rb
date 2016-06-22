c9f088c94237ad24e09f3493b9c8f799c1d19f6d
gautamsawhney <gsawhney21@gmail.com>
2016-06-22 04:40:59 -0400

    Generated API Routes for Comment

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
       resources :restaurants do
       end
       resources :posts do
+        resources :comments
       end
       resources :users do
       end
