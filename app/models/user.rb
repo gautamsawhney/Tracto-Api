986d6ff285a671baf6641c91712639167f57bf90
gautamsawhney <gsawhney21@gmail.com>
2016-06-22 04:38:24 -0400

    Making Email Non Mandatory for Social Logins

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,41 @@
 class User < ActiveRecord::Base
+  # Autocode: Callback
 
-	# Autocode: Callback
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
 
-	# Include default devise modules. Others available are:
-	# :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable
 
- 	devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable
   # Autocode: Relationships
-  has_many :authtokens, dependent: :destroy
 
-	# File Upload
+  has_many :authtokens, dependent: :destroy
+  has_many :identities, dependent: :destroy
+
+  # File Upload
 
   # Autocode: Validations
 
+  validates_presence_of   :email, if: :email_required?
+  validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
+  validates_format_of     :email, with: Devise.email_regexp, allow_blank: true, if: :email_changed?
+
+  validates_presence_of     :password, if: :password_required?
+  validates_confirmation_of :password, if: :password_required?
+  validates_length_of       :password, within: Devise.password_length, allow_blank: true
+
+  validates_presence_of     :password_confirmation, if: :password_required?
+  validates_confirmation_of :password_confirmation, if: :password_required?
+
 	# Soft Destroy
+
+	def password_required?
+    return false if email.blank?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
+
+  def email_required?
+    true
+  end
+
 end
