4187672b16854f2f3cf0034d71e45a082234be89
gautamsawhney <gsawhney21@gmail.com>
2016-06-22 04:39:02 -0400

    Added Relationships for models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,7 @@ class Restaurant < ApplicationRecord
   # Autocode: Callbacks
     
   # Autocode: Relationships
+  has_many :posts
       belongs_to :user
   
   # Autocode: Accept Nested Attributes
