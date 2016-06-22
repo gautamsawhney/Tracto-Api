ea4b0cfaca45627b8169eaf07efdb32b7d305df2
gautamsawhney <gsawhney21@gmail.com>
2016-06-22 06:19:17 -0400

    Added Discount model

---
+++ app/models/discount.rb
@@ -0,0 +1,15 @@
+class Discount < ApplicationRecord
+
+  # Autocode: Callbacks
+    
+  # Autocode: Relationships
+  
+  # Autocode: Accept Nested Attributes
+
+  # File Upload
+
+  # Autocode: Validations
+
+	# Soft Destroy
+
+end
